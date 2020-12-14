7c79863f383e72f9f90ee3740ee921eb30866412
corentinperroux <corentin7301@gmail.com>
2020-12-14 14:29:01 +0100

    favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,11 +3,11 @@ export default {
   ssr: false,
   target: 'static',
   head: {
-    title: 'Template NUXT',
+    title: 'OTAL',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Template NUXT starter kit description' }
+      { hid: 'description', name: 'description', content: 'Réseau social du digital' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
